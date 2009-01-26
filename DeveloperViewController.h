597a6d67c8cbf78ce3ef2e056a06ade23a8926e6
talha <talha.syed@kaboose.com>
2009-01-26 16:23:23 -0500

    [NEW] DeveloperViewController;

---
+++ DeveloperViewController.h
@@ -0,0 +1,18 @@
+//
+//  DeveloperViewController.h
+//  Developer
+//
+//  Created by Talha Syed on 26/01/09.
+//  Copyright 2009 RyeHigh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DeveloperViewController : UIViewController {
+	IBOutlet UITextView *developerDescription;
+}
+
+@property(nonatomic, retain) IBOutlet UITextView *developerDescription;
+
+@end
