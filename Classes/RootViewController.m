4c4a4a35e5a0c3652870d9675798df90a0a606e0
Talha Syed <talha@X1600RampagePro.local>
2009-01-28 00:54:46 -0500

    [NEW] (not fully working) delete cell capability;

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -69,19 +69,21 @@
 }
 
 
-/*
+
 // Override to support editing the list
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
+		DeveloperAppDelegate *appDelegate = (DeveloperAppDelegate *)[[UIApplication sharedApplication] delegate];
+		[appDelegate.developers removeObjectAtIndex:0];
+		NSLog(@"HEYYYY LOGGGING");
         [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
     }   
     if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
 
 
 /*
